edff66c5ec32897eb1541ccd4f9da214d88915f3
Mohammed Galal <MohammedGalal7777@hotmail.com>
2025-06-10 23:45:53 +0300

    Created ConfigDB (Database Config)

---
+++ ChatServer/src/main/java/com/chatsystem/server/config/ConfigDB.java
@@ -0,0 +1,7 @@
+package com.chatsystem.server.config;
+
+public class ConfigDB {
+    public static final String URL = "jdbc:mysql://localhost:3306/chat_system";
+    public static final String USER = "root";
+    public static final String PASSWORD = "";
+}
