030f53f57fe7125cd4f1e7317b2cd077a9adab90
Mohammed Galal <MohammedGalal7777@hotmail.com>
2025-06-14 17:15:13 +0300

    Add AuthController to manage user authentication actions

---
+++ ChatServer/src/main/java/com/chatsystem/server/Controller/AuthController.java
@@ -0,0 +1,54 @@
+package com.chatsystem.server.Controller;
+
+import com.chatsystem.server.Model.User;
+import com.chatsystem.server.network.Request;
+import com.chatsystem.server.network.Response;
+import com.chatsystem.server.services.AuthService;
+
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController() {
+        this.authService = new AuthService();
+    }
+
+    public Response executeRequest(Request request) {
+        switch (request.getAction()) {
+            case LOGIN:
+                return login(request);
+            case REGISTER:
+                return register(request);
+            case LOGOUT:
+                return logout(request);
+            default:
+                return new Response(false, "Invalid action", null);
+        }
+    }
+
+    // تسجيل الدخول
+    public Response login(Request request) {
+        User user = authService.login(request.getUser().getEmail(), request.getUser().getPasswordHash());
+        if (user != null) {
+            return new Response(true, "Login successful", user);
+        } else {
+            return new Response(false, "Invalid email or password", null);
+        }
+    }
+
+    // تسجيل مستخدم جديد
+    public Response register(Request request) {
+        boolean success = authService.register(request.getUser().getUsername(), request.getUser().getEmail(), request.getUser().getPasswordHash());
+        if (success) {
+            return new Response(true, "Registration successful");
+        } else {
+            return new Response(false, "Email or username already taken");
+        }
+    }
+
+    // تسجيل الخروج
+    public Response logout(Request request) {
+        int userId = request.getUser().getUserId();
+        authService.logout(userId);
+        return new Response(true, "Logout successful");
+    }
+}
