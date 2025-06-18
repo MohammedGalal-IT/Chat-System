6db039ca8c66462a711e75d802e9c3d5c9a9ac4b
Mohammed Galal <MohammedGalal7777@hotmail.com>
2025-06-18 21:08:14 +0300

    Enhance MessageController to include action details in responses and improve error handling

--- ChatServer/src/main/java/com/chatsystem/server/Controller/MessageController.java
+++ ChatServer/src/main/java/com/chatsystem/server/Controller/MessageController.java
@@ -33,34 +33,38 @@ public class MessageController {
             case DELETE_MESSAGE:
                 return deleteMessage(request);
             default:
-                return new Response(false, "Invalid Action");
+                return new Response(false, "Invalid Action", request.getAction());
         }
     }
 
     // Send a message
     private Response sendMessage(Request request) {
-        if(request.getMessage().getMessage_type() != Message.MessageType.TEXT){
-            return new Response(false, "Message type is not text");
+        try{
+            if(request.getMessage().getMessage_type() != Message.MessageType.TEXT){
+                return new Response(false, "Message type is not text", Action.SEND_MESSAGE);
             }
-        Message message = request.getMessage();
-        Response response = new Response();
+            Message message = request.getMessage();
         
-        message = messageService.sendMessage(message);
-        if (message != null) {
-            response.setSuccess(true);
-            response.setMessage("Message sent");
-             response.setMessageObj(message);
-             request.setAction(request.getAction());
-            return response;
-        } else {
-            return new Response(false, "Failed to send message");
+            message = messageService.sendMessage(message);
+            if (message != null) {
+                Response response = new Response(true, "Message sent", Action.SEND_MESSAGE);
+                response.setMessageObj(message);
+                return response;
+            } else {
+                System.out.println("No Returned Message");
+                return new Response(false, "Failed to send message", Action.SEND_MESSAGE);
+            }
+        } catch (Exception e){
+            System.out.println("Exception at sendMessage :" + e.getMessage());
+            return new Response(false, "Exception: Failed to send message ", Action.SEND_MESSAGE);
         }
+
     }
 
     // Send a message with attachment
     private Response sendMessageWithAttachment(Request request) {
         if(request.getMessage().getMessage_type() == Message.MessageType.TEXT){
-            return new Response(false, "Message type is not file");
+            return new Response(false, "Message type is not file", Action.SEND_MESSAGE_WITH_ATTACHMENT);
             }
         Message message = request.getMessage();
         Response response = new Response();
@@ -73,23 +77,39 @@ public class MessageController {
             request.setAction(request.getAction());
             return response;
         } else {
-            return new Response(false, "Failed to send message");
+            return new Response(false, "Failed to send message", Action.SEND_MESSAGE_WITH_ATTACHMENT);
         }
     }
 
     // Get messages between two users
     private Response getMessagesBetweenUsers(Request request) {
-        int userId1 = ((User) request.getData()[0]).getUser_id();
-        int userId2 = ((User) request.getData()[1]).getUser_id();
-        List<Message> messages = messageService.getMessagesBetweenUsers(userId1, userId2);
-        if (messages != null && !messages.isEmpty()) {
-            Response response = new Response(true, "Messages found");
-            response.setMessages(messages);
-            response.setAction(request.getAction());
-            return response;
-        } else {
-            return new Response(false, "No messages found");
+        if(request.getData() == null){
+            return new Response(false, "No Data Sent", Action.GET_MESSAGES_BETWEEN_USERS);
         }
+        try{
+            int userId1 = Integer.parseInt((String) request.getData()[0]);
+            int userId2 = Integer.parseInt((String) request.getData()[1]);
+            List<Message> messages = messageService.getMessagesBetweenUsers(userId1, userId2);
+            if (messages != null && !messages.isEmpty()) {
+                for (Message message : messages) {
+                    if(request.getUser().getUser_id() == message.getReceiver_id() && !message.is_read){
+                        message.setIs_read(true);
+                        messageService.updateMessage(message);
+                    }
+                }
+                Response response = new Response(true, "Messages found", Action.GET_MESSAGES_BETWEEN_USERS);
+                response.setMessages(messages);
+                
+                return response;
+            } else {
+                return new Response(true, "No messages found", Action.GET_MESSAGES_BETWEEN_USERS);
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return new Response(false, "Exception: " + e.getMessage(), Action.GET_MESSAGES_BETWEEN_USERS);
+        }
+
     }
 
     // Get unread messages for a user
@@ -97,12 +117,12 @@ public class MessageController {
         int userId = ((User) request.getData()[0]).getUser_id();
         List<Message> messages = messageService.getUnreadMessages(userId);
         if (messages != null && !messages.isEmpty()) {
-            Response response = new Response(true, "Unread messages found");
+            Response response = new Response(true, "Unread messages found", Action.GET_UNREAD_MESSAGES);
             response.setMessages(messages);
             response.setAction(request.getAction());
             return response;
         } else {
-            return new Response(false, "No unread messages");
+            return new Response(false, "No unread messages", Action.GET_UNREAD_MESSAGES);
         }
     }
 
@@ -123,7 +143,7 @@ public class MessageController {
         MessageType type  = ((Message) request.getData()[1]).getMessage_type();
         List<Message> messages = messageService.getMessagesByType(userId, type);
         if (messages != null && !messages.isEmpty()) {
-            Response response = new Response(true, "Messages found");
+            Response response = new Response(true, "Messages found", Action.GET_MESSAGES_BY_TYPE);
             response.setMessages(messages);
             response.setAction(request.getAction());
             return response;
@@ -135,7 +155,7 @@ public class MessageController {
     // Delete a message
     private Response deleteMessage(Request request) {
         if(request.getData() == null || request.getData().length == 0 || !(request.getData()[0] instanceof Message)){
-            return new Response(false, "Invalid request data");
+            return new Response(false, "Invalid request data", Action.DELETE_MESSAGE);
         }
         int messageId = ((Message) request.getData()[0]).getMessage_id();
         boolean success = messageService.deleteMessage(messageId);
